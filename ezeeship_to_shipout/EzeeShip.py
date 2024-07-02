3682813e68168fba19c82c27f9d3cd1f291184c0
Ben Xie <ben@carrohome.com>
2024-07-02 15:57:06 -0700

    Nothing

--- ezeeship_to_shipout/EzeeShip.py
+++ ezeeship_to_shipout/EzeeShip.py
@@ -10,7 +10,7 @@ from webdriver import wait_for_element
 current_dir = os.path.dirname(os.path.abspath(__file__))
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
-options.add_argument('--headless')
+# options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dve-shm-uage')
 prefs = {"download.default_directory": current_dir}
